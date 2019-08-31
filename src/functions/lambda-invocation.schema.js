70939eafa538ac93a904adacc69486a1285561ea
theburningmonk <theburningmonk@gmail.com>
2019-08-31 18:44:14 +0100

    test: added tests for lambda-invocation

--- src/functions/lambda-invocation.schema.js
+++ src/functions/lambda-invocation.schema.js
@@ -1,6 +1,6 @@
 const Joi = require("@hapi/joi");
 
-const ARN = Joi.string().regex(/arn:aws:lambda/g);
+const ARN = Joi.string().regex(/arn:aws:lambda/);
 
 const LambdaInvocation = Joi.object().keys({
 	FunctionName: ARN.required(),
