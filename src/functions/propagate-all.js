9ee7ddc7c4d0bc90acc3e4cda5e6a1d4f7c8f46c
theburningmonk <theburningmonk@gmail.com>
2019-08-31 22:59:41 +0100

    feat: propagate-all no longer recurses

--- src/functions/propagate-all.js
+++ src/functions/propagate-all.js
@@ -1,36 +1,19 @@
 const _ = require("lodash");
 const log = require("@dazn/lambda-powertools-logger");
-const lambda = require("./lib/lambda");
 const cloudFormation = require("./lib/cloudformation");
 const { propagateTags } = require("./lib/propagate-tags");
 
-const ONE_MINUTE = 60 * 1000;
-let stackNames = [];
-
-module.exports.handler = async (event, context) => {
+module.exports.handler = async (event) => {
 	log.debug("received event...", { event });
   
-	if (!_.isEmpty(stackNames)) {
-		log.info("continuing from last recursion...", { 
-			remainingCount: stackNames.length
-		});
-	} else {
-		stackNames = await cloudFormation.listStacks();
-		log.info("loaded stacks...", {
-			remainingCount: stackNames.length
-		});
-	}
+	const stackNames = await cloudFormation.listStacks();
+	log.info("loaded stacks...", {
+		count: stackNames.length
+	});
   
-	do {
+	while (!_.isEmpty(stackNames)) {
 		const stackName = stackNames.pop();
 		await propagateTags(stackName);
 		log.info("processed stack...", { stackName });
-	} while (!_.isEmpty(stackNames) && context.getRemainingTimeInMillis() > ONE_MINUTE);
-
-	if (!_.isEmpty(stackNames)) {
-		await lambda.recurse(event);
-		return "to be continued...";
-	} else {
-		return "the end...";
 	}
 };
--- src/functions/propagate-all.test.js
+++ src/functions/propagate-all.test.js
@@ -1,4 +1,3 @@
-const AWS = require("./lib/aws");
 const cloudFormation = require("./lib/cloudformation");
 const propagateTags = require("./lib/propagate-tags");
 
@@ -6,9 +5,6 @@ const propagateTags = require("./lib/propagate-tags");
 // so no need to test it here as well
 const mockListStacks = jest.fn();
 cloudFormation.listStacks = mockListStacks;
-
-const mockInvoke = jest.fn();
-AWS.Lambda.prototype.invoke = mockInvoke;
 
 // propagateTags is tested by the propagate.test.js module already
 // so no need to test it here as well
@@ -20,11 +16,10 @@ console.log = jest.fn();
 afterEach(() => {
 	mockListStacks.mockClear();
 	mockPropagateTags.mockClear();
-	mockInvoke.mockClear();
 });
 
 describe("propagate-all", () => {
-	test("processes all stacks returned by listStacks when there are sufficient time", async () => {
+	test("processes all stacks returned by listStacks", async () => {
 		givenListStacksReturns(["stack1", "stack2", "stack3"]);
     
 		givenPropagateTagsSucceeds(); // stack1
@@ -36,30 +31,6 @@ describe("propagate-all", () => {
     
 		expect(mockListStacks).toBeCalled();
 		expect(mockPropagateTags).toBeCalledTimes(3);
-		expect(mockInvoke).not.toBeCalled();
-	});
-  
-	test("recurses if there are insufficient time", async () => {
-		givenListStacksReturns(["stack1", "stack2", "stack3"]);
-    
-		givenPropagateTagsSucceeds(); // stack1
-		givenPropagateTagsSucceeds(); // stack2
-		givenPropagateTagsSucceeds(); // stack3
-    
-		givenRecursionSucceeds();
-    
-		const handler = require("./propagate-all").handler;
-		await handler({}, { getRemainingTimeInMillis: () => 1000 });
-    
-		expect(mockListStacks).toBeCalledTimes(1);
-		expect(mockPropagateTags).toBeCalledTimes(1);
-		expect(mockInvoke).toBeCalled();
-    
-		// recurse, now there are more time!
-		await handler({}, { getRemainingTimeInMillis: () => 900000 });
-    
-		expect(mockListStacks).toBeCalledTimes(1);
-		expect(mockPropagateTags).toBeCalledTimes(3);
 	});
 });
 
@@ -69,9 +40,4 @@ function givenListStacksReturns(stackNames) {
 
 function givenPropagateTagsSucceeds() {
 	mockPropagateTags.mockResolvedValueOnce();
-}
-function givenRecursionSucceeds() {
-	mockInvoke.mockReturnValueOnce({
-		promise: () => Promise.resolve()
-	});
 }
