7ab77c52ca042d99c8a5524d2a5f74c08c9d3152
theburningmonk <theburningmonk@gmail.com>
2019-08-31 23:49:24 +0100

    feat: support retries

--- src/functions/propagate-all.js
+++ src/functions/propagate-all.js
@@ -2,6 +2,7 @@ const _ = require("lodash");
 const log = require("@dazn/lambda-powertools-logger");
 const cloudFormation = require("./lib/cloudformation");
 const { propagateTags } = require("./lib/propagate-tags");
+const retry = require("async-retry");
 
 module.exports.handler = async (event) => {
 	log.debug("received event...", { event });
@@ -12,8 +13,18 @@ module.exports.handler = async (event) => {
 	});
   
 	while (!_.isEmpty(stackNames)) {
-		const stackName = stackNames.pop();
-		await propagateTags(stackName);
+		const stackName = stackNames.pop();        
+		await retry(async () => propagateTags(stackName), {
+			retries: 10,
+			// always wait 1 min between retry to give the API bucket
+			// a chance to refill
+			minTimeout: 60000,
+			maxTimeout: 60000,
+			factor: 1,
+			onRetry: (err) => {
+				log.warn("retrying after error...", err);
+			}
+		});
 		log.info("processed stack...", { stackName });
 	}
 };
